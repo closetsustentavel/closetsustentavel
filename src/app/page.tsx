a6740f4082ad2f4e301cc9cb5d712aa70a7d2309
Davi Belo <davibelocontato@gmail.com>
2025-07-03 17:33:34 -0300

    section/about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 //import Image from "next/image";
 import { Header } from "@/components/Header/Header";
 import { Hero } from "@/components/Hero/Hero";
+import { About } from "@/components/About/About";
 export default function Home() {
   return (
     <div>
       <Header/>
       <Hero/>
+      <About/>
     </div>
   );
 }
---
+++ src/components/About/About.tsx
@@ -0,0 +1,31 @@
+"use client";
+import Shop from "@/assets/window_shop_closet.svg";
+import Image from "next/image";
+export function About() {
+  return (
+    <div className="flex flex-row ml-[255px] mt-50 w-[700px] h-full gap-5">
+      <div className="flex flex-col">
+        <h3 className="text-[52px] mb-[70px]">Quem somos</h3>
+        <div className="">
+          <p className="pb-5">
+            O Closet Sustentável é um brechó com consultoria de imagem
+            especializada, oferecendo uma curadoria contemporânea e atemporal.
+          </p>
+          <p className="pb-5">
+            O atendimento é consultivo e personalizado, realizado por Ju
+            Rezende, Consultora de Imagem.
+          </p>
+          <p className="pb-5">
+            Além de encontrar peças de qualidade selecionadas com cuidado, você
+            também pode contar com os serviços de consultoria de imagem,
+            investindo em autoconhecimento para aprender a consumir de forma
+            mais consciente, sustentável e alinhada ao seu estilo pessoal.
+          </p>
+        </div>
+      </div>
+      <div className="h-full w-[800px] flex mt-auto">
+        <Image src={Shop} alt="Fachada Loja" />
+      </div>
+    </div>
+  );
+}
