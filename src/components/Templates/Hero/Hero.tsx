e8cfbffd31337963f7caa8e0f5472b92f45780ca
Davi Belo <davibelocontato@gmail.com>
2025-07-24 18:43:52 -0300

    fixed hero

--- src/components/Templates/Hero/Hero.tsx
+++ src/components/Templates/Hero/Hero.tsx
@@ -3,7 +3,7 @@ import DownArrow from "@/assets/downArrow.gif";
 import Image from "next/image";
 export function Hero() {
   return (
-    <div>
+    <section>
       <div className="text-center mt-9 ml-0 md:ml-12 lg:ml-[100px] md:text-start">
         <h1
           className="text-[40px] leading-none my-0 text-wrap md:text-6xl lg:text-7xl xl:text-8xl 3xl:text-[150px]"
@@ -27,6 +27,6 @@ export function Hero() {
       <div className="flex justify-center items-center pt-20 sm:hidden">
         <Image src={DownArrow} alt="knowMOre" height={15}></Image>
       </div>
-    </div>
+    </section>
   );
 }
